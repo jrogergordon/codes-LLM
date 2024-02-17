e5c0ff37cf5a0fa162a3b5cbbd19b5835d2485e3
Jacob <jrogergordon@gmail.com>
2024-02-16 23:57:41 -0500

    rotate

---
+++ python/test.py
@@ -0,0 +1,11 @@
+def rotate_matrix(matrix):
+    n, m = len(matrix), len(matrix[0])
+    for i in range(n):
+        for j in range(m):
+            if i < m and j < n:
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[j][-i-1]
+                matrix[j][-i-1] = temp
+    return matrix
+ans = rotate_matrix([[1,2,3, 4],[5,6,7,8],[9,10,11,12]])
+print(ans)
