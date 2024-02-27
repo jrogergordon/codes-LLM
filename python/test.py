7efd248cca1690f72a5f5cc2d178f747ba210816
Jacob <jrogergordon@gmail.com>
2024-02-26 20:50:11 -0500

    dynamic k deletions palindrome

--- python/test.py
+++ python/test.py
@@ -1,33 +1,19 @@
-def numIslands(grid):
-    if not grid:
-        return 0, 0, 0
+def can_form_palindrome(s, k):
+    if len(s) < k:
+        return True
 
-    count = 0
-    max_size = 0
-    distinct_islands = set()
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == '1':
-                shape = dfs(grid, i, j)
-                count += 1
-                max_size = max(max_size, len(shape))
-                distinct_islands.add(shape)
-    return count, max_size, len(distinct_islands)
+    l = 0
+    r = len(s) - 1
+    delete_count = 0
 
-def dfs(grid, i, j):
-    if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
-        return ''
-    grid[i][j] = '#'
-    shape = f'{i},{j}'
-    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    for dx, dy in directions:
-        shape += dfs(grid, i+dx, j+dy)
-    return shape
+    while l < r:
+        if s[l] != s[r]:
+            delete_count += 1
+            if delete_count > k:
+                return False
+        l += 1
+        r -= 1
 
-ans = numIslands([
-  ["1","1","1","1","0"],
-  ["1","1","0","1","0"],
-  ["1","1","0","0","1"],
-  ["0","0","1","0","0"]
-])
+    return True
+ans = can_form_palindrome("leseli", 1)
 print(ans)
