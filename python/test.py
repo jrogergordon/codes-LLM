28de24f49bdf0dd7e47287dd4d39c0f2da6f2f30
Jacob <jrogergordon@gmail.com>
2024-03-09 13:46:30 -0500

    sql

--- python/test.py
+++ python/test.py
@@ -1,23 +1,19 @@
-def lengthOfLongestSubstringTwoSame(s):
-    n = len(s)
-    ans = 0
-    left = 0
-    count = [0] * 26
-    same = 0
+def check_characters(needle, haystack):
+    # Create a dictionary to store the count of each character in haystack
+    char_count = {}
+    for char in haystack:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    
+    # Check each character in needle
+    for char in needle:
+        if char in char_count and char_count[char] > 0:
+            char_count[char] -= 1
+        else:
+            return -1
+    
+    return True
 
-    for right in range(n):
-        count[s[right] - 'a'] += 1
-        if count[s[right] - 'a'] == 3:
-            same += 1
-
-        while same > 0:
-            count[s[left] - 'a'] -= 1
-            if count[s[left] - 'a'] == 2:
-                same -= 1
-            left += 1
-
-        ans = max(ans, right - left + 1)
-
-    return ans
-
-print(lengthOfLongestSubstringTwoSame("aabbczlkdtrrrrrrrrrddddddddd"))
+print(check_characters("aaaaaaaaa", "aaaaaaaa"))
