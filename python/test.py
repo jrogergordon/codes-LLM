756172c9e1491c3ef2f64a666fe6f258bd101aa1
Jacob <jrogergordon@gmail.com>
2024-03-11 23:49:42 -0400

    sql

--- python/test.py
+++ python/test.py
@@ -1,19 +1,13 @@
-def check_characters(needle, haystack):
-    # Create a dictionary to store the count of each character in haystack
-    char_count = {}
-    for char in haystack:
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
-    
-    # Check each character in needle
-    for char in needle:
-        if char in char_count and char_count[char] > 0:
-            char_count[char] -= 1
-        else:
-            return -1
-    
-    return True
+def get_rotations(l1, l2):
+    if sorted(l1) != sorted(l2):
+        return -1
+    else:
+        for i in range(len(l1)):
+            if all(l1[j] == l2[(i+j)%len(l1)] for j in range(len(l1))):
+                return i
+        return -1
 
-print(check_characters("aaaaaaaaa", "aaaaaaaa"))    
+l1 = [1, 2, 3, 4, 5, 1, 1]
+l2 = [2, 3, 4,5,1,1,1]
+
+print(get_rotations(l1, l2))  # output: 2uts: (2, 3)
