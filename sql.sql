2f48ac75b6e04847869248b3ba990536f9084df9
Jacob <jrogergordon@gmail.com>
2024-03-17 23:57:28 -0400

    price_change_sql

--- sql.sql
+++ sql.sql
@@ -1,3 +1,12 @@
-SELECT product_id
-FROM Products
-WHERE low_fats = 'Y' AND recyclable = 'Y';
+SELECT 
+    P1.product_id,
+    COALESCE(P2.new_price, 10) AS price
+FROM 
+    Products P1
+LEFT JOIN 
+    Products P2
+ON 
+    P1.product_id = P2.product_id AND 
+    P2.change_date <= '2019-08-16'
+GROUP BY 
+    P1.product_id;
