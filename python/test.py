42be31f1f6cfcdb3a2986b18faaaf3f890ea4e05
Jacob <jrogergordon@gmail.com>
2024-03-27 23:53:45 -0400

    css

--- python/test.py
+++ python/test.py
@@ -1,14 +1,25 @@
-def findShortest(s):
-    n = len(s)
-    if (n == 0):
-        return 0
-    dp = [[0 for x in range(n + 1)] for x in range(n + 1)]
-    for i in range(n + 1):
-        dp[i][0] = i
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            if (s[i - 1] == s[j - 1] and i != j):
-                dp[i][j] = dp[i - 1][j - 1]
-            else:
-                dp[i][j] = min(dp[i][j - 1], dp[i - 1][j] + 1)
-    return dp[n][n]
+
+
+def n_knights(n):
+        
+
+    def place_knights():
+        col = 0
+        while col < n:
+            # Fill every third column using pattern A
+            for row in range(0, n, 2):
+                chessboard[row][col] = 1
+            col += 3
+
+            if col < n:  # Fill every third column using pattern B
+                for row in range(1, n, 2):
+                    chessboard[row][col] = 1
+            col += 3
+
+    chessboard = [[0] * n for _ in range(n)]
+    place_knights()
+    return chessboard
+
+solution = n_knights(4)  # Example for an 8x8 board
+for row in solution:
+    print(row)
