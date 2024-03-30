7d7a1e756b61f6f6cb288630733060e467c80aa7
Jacob <jrogergordon@gmail.com>
2024-03-30 18:13:05 -0400

    restaurant sql

--- python/sql.sql
+++ python/sql.sql
@@ -8,3 +8,12 @@ WITH daily_amount AS (
     GROUP BY 
         visited_on
 )
+SELECT 
+    visited_on, 
+    ROUND(AVG(amount) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) AS average_amount
+FROM 
+    daily_amount
+WHERE 
+    row_num > 6
+ORDER BY 
+    visited_on;
