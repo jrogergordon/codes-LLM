9101837b99a7ae5b5c4a6646ab537db8c89356c6
Jacob <jrogergordon@gmail.com>
2024-03-30 18:13:33 -0400

    restaurant sql

--- python/sql.sql
+++ python/sql.sql
@@ -2,7 +2,7 @@ WITH daily_amount AS (
     SELECT 
         visited_on, 
         SUM(amount) AS amount,
-        ROW_NUMBER() OVER (ORDER BY visited_on) AS row_num 
+        ROW_NUMBER() OVER (ORDER BY visited_on) AS row_num  
     FROM 
         Customer
     GROUP BY 
