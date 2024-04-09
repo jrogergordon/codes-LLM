0206a4602313effe51f113bcf5af6853e09fddab
Jacob <jrogergordon@gmail.com>
2024-04-08 20:09:04 -0400

    time testing

--- python/test.py
+++ python/test.py
@@ -1,19 +1,7 @@
-import folium
+import time
 
-# Coordinates (latitude and longitude)
-locations = [[40.7128, -74.0060, 'New York'], 
-             [34.0522, -118.2437, 'Los Angeles'], 
-             [41.8781, -87.6298, 'Chicago']]
+def add_time_to_file(file_name):
+    with open(file_name, 'w') as f:
+        f.write(time.ctime() + "\n")
 
-# Create the map
-m = folium.Map(location=[39.8283, -98.5795], zoom_start=4)
-
-# Add markers with custom icons and popups
-for loc in locations:
-    folium.Marker(
-        location=[loc[0], loc[1]],
-        popup=f'<strong>{loc[2]}</strong>',
-        icon=folium.Icon(icon='cloud')
-    ).add_to(m)
-
-m.save('map_advanced.html')
+add_time_to_file('example.txt')
